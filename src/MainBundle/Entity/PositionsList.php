df1be7640a697d1e3e8b7437061085ddb151a9a1
ARTUR <artur.tovmasyan777@gmail.com>
2015-09-30 10:47:03 +0400

    create positionsList admin and entity

---
+++ src/MainBundle/Entity/PositionsList.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * PositionsList
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class PositionsList
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="position", type="string", length=255)
+     */
+    private $position;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="salary", type="integer")
+     */
+    private $salary;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set position
+     *
+     * @param string $position
+     * @return PositionsList
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    /**
+     * Get position
+     *
+     * @return string 
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return PositionsList
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set salary
+     *
+     * @param integer $salary
+     * @return PositionsList
+     */
+    public function setSalary($salary)
+    {
+        $this->salary = $salary;
+
+        return $this;
+    }
+
+    /**
+     * Get salary
+     *
+     * @return integer 
+     */
+    public function getSalary()
+    {
+        return $this->salary;
+    }
+}
